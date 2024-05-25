247e4ee7a5fb2afcdbcf106f3fe2e61e2cf54ed8
minaemad13 <menaemadorai@gmail.com>
2024-05-26 00:07:45 +0300

    init commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,208 @@
+import React, { useState } from "react";
+import { Link as LinkR } from "react-router-dom";
+import styled, { useTheme } from "styled-components";
+import { Bio } from "../data/constants";
+import { MenuRounded } from "@mui/icons-material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import PersonPinIcon from "@mui/icons-material/PersonPin";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import mlogo from "../images/M_LOGO.png";
+
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  color: white;
+`;
+
+
+const NavbarContainer = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  padding: 0 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+`;
+const NavLogo = styled(LinkR)`
+  display: flex;
+  align-items: center;
+  width: 80%;
+  padding: 0 6px;
+  font-weight: 500;
+  font-size: 18px;
+  text-decoration: none;
+  color: inherit;
+`;
+
+const NavItems = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0 6px;
+  list-style: none;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavLink = styled.a`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const ButtonContainer = styled.div`
+  width: 80%;
+  height: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  padding: 0 6px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileIcon = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const MobileMenu = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 16px;
+  padding: 0 6px;
+  list-style: none;
+  width: 100%;
+  padding: 12px 40px 24px 40px;
+  background: ${({ theme }) => theme.card_light + 99};
+  position: absolute;
+  top: 80px;
+  right: 0;
+
+  transition: all 0.6s ease-in-out;
+  transform: ${({ isOpen }) =>
+    isOpen ? "translateY(0)" : "translateY(-100%)"};
+  border-radius: 0 0 20px 20px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  z-index: ${({ isOpen }) => (isOpen ? "1000" : "-1000")};
+`;
+const NavImage = styled.img`
+  width: 48px;
+  height: 48px;
+`;
+
+const IconLink = styled.a`
+  display: inline-block;
+  margin: 0 1rem;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.text_primary};
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const theme = useTheme();
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">
+          
+          <NavImage src={mlogo} />
+
+        </NavLogo>
+
+        <MobileIcon onClick={() => setIsOpen(!isOpen)}>
+          <MenuRounded style={{ color: "inherit" }} />
+        </MobileIcon>
+
+        <NavItems>
+          <NavLink href="#About">About</NavLink>
+          <NavLink href="#Skills">Skills</NavLink>
+          <NavLink href="#Experience">Experience</NavLink>
+          <NavLink href="#Projects">Projects</NavLink>
+          <NavLink href="#Education">Education</NavLink>
+        </NavItems>
+
+        {isOpen && (
+          <MobileMenu isOpen={isOpen}>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#About">
+              About
+            </NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Skills">
+              Skills
+            </NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Experience">
+              Experience
+            </NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Projects">
+              Projects
+            </NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Education">
+              Education
+            </NavLink>
+            <IconLink href={Bio.github} target="display">
+              <GitHubIcon  />
+            </IconLink>
+            <IconLink href={Bio.linkedin} target="display">
+              <LinkedInIcon  />
+            </IconLink>
+            <IconLink href={Bio.whatsapp} target="display">
+              <WhatsAppIcon  />
+            </IconLink>
+            <IconLink href={Bio.facebook} target="display">
+              <PersonPinIcon  />
+            </IconLink>
+          </MobileMenu>
+        )}
+
+        <ButtonContainer>
+          <IconLink href={Bio.github} target="display">
+            <GitHubIcon  />
+          </IconLink>
+          <IconLink href={Bio.linkedin} target="display">
+            <LinkedInIcon  />
+          </IconLink>
+          <IconLink href={Bio.whatsapp} target="display">
+            <WhatsAppIcon  />
+          </IconLink>
+          <IconLink href={Bio.resume} target="display">
+            <PersonPinIcon  />
+          </IconLink>
+        </ButtonContainer>
+      </NavbarContainer>
+    </Nav>
+  );
+};
+
+export default Navbar;
---
+++ src/data/constants.js
@@ -0,0 +1,520 @@
+import reactjs from "../images/reactjs.svg";
+import redux from "../images/redux.svg";
+import tailwind from "../images/tailwind.svg";
+import matrialui from "../images/matrialui.png";
+import JavaScript from "../images/JavaScript.png";
+import HTML5 from "../images/HTML5.png";
+import CSS3 from "../images/CSS3.png";
+import bootstrap from "../images/bootstrap.png";
+import Django from "../images/Django.png";
+import flask from "../images/flask.png";
+import python from "../images/pyhon.svg";
+import oracle from "../images/oracle-svgrepo-com.svg";
+import postgresql from "../images/postgresql.png";
+import mysql from "../images/mysql.svg";
+import mongo from "../images/nongo.svg";
+import Express from "../images/express.png";
+import redis from "../images/redis-logo-svgrepo-com.svg";
+import restapi from "../images/rest-api-icon.svg";
+import nginx from "../images/NGINX.png";
+import apache from "../images/apache-svgrepo-com.svg";
+import docker from "../images/docker.svg";
+import AWS from "../images/AWS.png";
+import certbot from "../images/certbot-svgrepo-com.svg";
+import git from "../images/git.png";
+import githup from "../images/Githup.png";
+import gitlab from "../images/gitlab-svgrepo-com.svg";
+import vscode from "../images/vs-code-svgrepo-com.svg";
+import postman from "../images/postman.png";
+import adobexd from "../images/adobe-xd-logo-svgrepo-com.svg";
+import figma from "../images/figma.png";
+import streamsets from "../images/streamsets.jpeg";
+import Vodafone from "../images/vodafone-icon.svg";
+import cob from "../images/COB.jpeg";
+import vegadata from "../images/vega.jpeg";
+import iti from "../images/ITI.png";
+import cairouni from "../images/Cairouni.svg";
+
+
+
+export const Bio = {
+  name: "Mina Emad",
+  roles: [
+    "Full Stack Developer",
+    "Backend Python Engineer",
+    "Software Engineer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/minaemad13", //"https://github.com/rishavchanda",
+  resume:
+    "https://drive.google.com/file/d/1b5qnrejLMcg1HVIEzKi7xRkgBI3thIuw/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/mina-emad-dev/",
+  whatsapp: "https://wa.me/+201061459068",
+  insta: "https://www.instagram.com/rishav_chanda/",
+};
+
+export const titlesOfSections = {
+  skills: "Here are some of my skills on which I have been working on for the past 3 years.",
+  
+};
+
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image: reactjs
+          
+      },
+      // {
+      //   name: "Redux",
+      //   image:
+      //     "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      // },
+      
+      {
+        name: "HTML5",
+        image: HTML5
+      },
+      {
+        name: "CSS3",
+        image: CSS3
+      },
+      {
+        name: "JavaScript",
+        image:
+          JavaScript
+      },
+      {
+        name: "Bootstrap",
+        image:
+          bootstrap
+      },
+      {
+        name: "Material UI",
+        image:
+          matrialui
+      },
+      {
+        name: "Tailwind",
+        image:tailwind
+          
+      },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Express Js",
+        image:Express
+      },
+      {
+        name: "Rest API",
+        image: restapi
+      },
+      {
+        name: "Python",
+        image:python
+      },
+      
+      {
+        name: "Django",
+        image:Django
+      },
+      {
+        name: "Flask",
+        image:flask
+      },
+      {
+        name: "MySQL",
+        image:mysql
+      },
+      {
+        name: "Postgresql",
+        image: postgresql
+      },
+      {
+        name: "MongoDB",
+        image:mongo
+      },
+      {
+        name: "Oracle",
+        image:oracle
+      },
+      {
+        name: "Redis",
+        image: redis
+      },
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image:AWS
+      },
+      
+      {
+        name: "Docker",
+        image:docker
+      },
+      
+      {
+        name: "Nginx",
+        image: nginx
+      },
+      
+      {
+        name: "Apache",
+        image: apache
+      },
+      {
+        name: "CertBot",
+        image: certbot
+      },
+    ],
+  },
+ 
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:git
+      },
+      {
+        name: "GitHub",
+        image:githup
+      },
+      {
+        name: "Git Lab",
+        image:gitlab
+      },
+      {
+        name: "VS Code",
+        image:vscode
+      },
+      {
+        name: "Postman",
+        image:postman
+      },
+      {
+        name: "Adobe XD",
+        image:adobexd
+      },
+      {
+        name: "Figma",
+        image:figma
+      },
+      {
+        name: "StreamSets",
+        image:streamsets
+      },
+    ],
+  },
+];
+
+
+
+export const experiences = [
+  {
+    id: 0,
+    img: Vodafone,
+    role: "Software Billing Engineer",
+    company: "Vodafone",
+    date: "Nov 2022 - Present",
+    desc: "• The Billing Engineer manages Vodafone's billing systems, ensuring accuracy, efficiency, and compliance. They focus on technological upgrades, regulatory compliance, and customer communication. Their collaborative approach with cross-functional teams contributes to Vodafone's success and customer satisfaction.",
+    skills: [
+      "Java",
+      "Python",
+      "SQL",
+      "PL/SQL",
+      "HTML5",
+      "CSS3",
+      "JavaScript",
+      "Oracle",
+      'linux',
+      'Bash'
+    ],
+  },
+  {
+    id: 1,
+    img: cob,
+    role: "DevOps & Fullstack Python Engineering 'Part-Time'",
+    company: "Conference Organizing Bureau 'COB'",
+    date: "Jun 2022 - Present",
+    desc: "• In my role as a DevOps & Fullstack Python Engineer, I effectively combined my expertise in software development and operations to streamline deployment processes and enhance system reliability. Leveraging Python's versatility, I crafted scalable solutions, fostering efficient communication between development and operations teams, ultimately driving product success.",
+    skills: [
+      "Python",
+      "Django",
+      "Docker",
+      "AWS",
+      "EC2",
+      "Nginx",
+      "JavaScript",
+      'Postgresql',
+      'Mysql'
+    ],
+  },
+  {
+    id: 2,
+    img: vegadata,
+    role: "Python Backend Engineer",
+    company: "Vega Data",
+    date: "Nov 2021 - Nov 2022",
+    desc: "• As a Python Backend Engineer, I specialized in crafting robust server-side applications, leveraging Python's flexibility and efficiency. My responsibilities included developing scalable backend systems, optimizing database performance, and ensuring seamless integration with front-end components. Through meticulous coding and problem-solving, I contributed to the creation of resilient and high-performing software solutions.",
+    skills: [
+      "Python",
+      "Django",
+      "Ransher",
+      "Docker",
+      "Linux",
+      "Nginx",
+      "StreamSets",
+      "Mapbox",
+    ],
+  },
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: iti,
+    school: "Information Technology Institute - ITI",
+    date: "Nov 2021 - Feb 2022",
+    grade: "Good",
+    desc: "I completed the ITI Track in Open Source, gaining comprehensive knowledge in open-source technologies and software development. This program equipped me with skills in Linux, Python, web development, and database management, fostering a strong foundation in building and managing open-source projects effectively.",
+    degree: "Full-Stack Web Development Using Python Track",
+  },
+  {
+    id: 1,
+    img: cairouni,
+    school: "Cairo University",
+    date: "Oct 2017 - May 2021",
+    grade: "Good",
+    desc: "I hold a degree in Computer Science, where I developed a solid foundation in algorithms, data structures, software engineering, and system design. My education provided hands-on experience in programming languages, database management, and software development methodologies, preparing me for diverse challenges in the tech industry.",
+    degree: "Computer Science",
+  },
+
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "DecisionHub",
+    date: "Jan 2024 - Dec 2023",
+    description:
+      "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and visualize decision strategies. Provide a no-code rule writing experience and visual representation to test these rules in real-time and observe the calculations at each step.",
+    image:
+      "https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg",
+    tags: [
+      "React Js",
+      "PostgressSQL",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "React Flow",
+    ],
+    category: "web app",
+    github: "https://github.com/rishavchanda/DecisionHub",
+    webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 9,
+    title: "Trackify",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "Docker",
+      "AWS",
+      "DuckDNS",
+      "Eslint",
+      "Husky",
+      "CI/CD",
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+    ],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Trackify",
+    webapp: "https://trackify-management.netlify.app/",
+  },
+  {
+    id: 0,
+    title: "Podstream",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Podstream",
+    webapp: "https://podstream.netlify.app/",
+    member: [
+      {
+        name: "Rishav Chanda",
+        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Vexa",
+    date: "Oct 2022 - Jan 2023",
+    description:
+      "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "NodeMailer",
+    ],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Project-Management-App",
+    webapp: "https://vexa-app.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Brain Tumor Detection",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Preprocessed and augmented the dataset to improve model accuracy, trained the model, created API using model and Python, and used React web app for the project's front end. Achievements: Achieved an accuracy of 99.2% to accurately detect brain tumors from medical images.",
+    image:
+      "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Brain-Tumor-Detection",
+    webapp: "https://brain-tumor.netlify.app/",
+    member: [
+      {
+        name: "Rishav Chanda",
+        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Buckoid",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "App Is Currently In Playstore 100+ Downloads. This Project proposes an “Expense Tracking App”. Keep track of your personal expenses and compare them to your monthly income with the budget planner. It has Google Drive Cloud API for Backup of User Room Database. Made with Kotlin in MVVM Architecture & Live Data.",
+    image:
+      "https://camo.githubusercontent.com/3ad28aa710d18525f1fc87de056ed53c706d09979589bfd5a773df36653bad38/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
+    tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
+    category: "android app",
+    github: "https://github.com/rishavchanda/Buckoid-Android-App",
+    webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  },
+  {
+    id: 10,
+    title: "Job Finding App",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
+    tags: ["React Native", "JavaScript", "Axios"],
+    category: "android app",
+    github: "https://github.com/rishavchanda/Job-finder-App",
+    webapp: "https://github.com/rishavchanda/Job-finder-App",
+  },
+  {
+    id: 4,
+    title: "Whatsapp Clone",
+    date: "Jul 2021",
+    description:
+      "A WhatsApp clone made with React JS, Firebase, and Material UI. It has Phone Authentication, Real-time Database. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
+    tags: ["React Js", "Firebase", "Firestore", "Node JS"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Whatsapp-Clone-React-Js",
+    webapp: "https://whatsapp-clone-rishav.web.app",
+  },
+  {
+    id: 5,
+    title: "Todo Web App",
+    date: "Jun 2021",
+    description:
+      " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+    image:
+      "https://camo.githubusercontent.com/84ac6ab6f378348ef28d8184062b7e9e3511a1252ae3966eaa49e8e998f732a7/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+    tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Todo-Web-App",
+    webapp: "https://rishav-react-todo.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "Breaking Bad",
+    date: "Jun 2021",
+    description:
+      "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
+    image:
+      "https://camo.githubusercontent.com/937774368308a82419f53dd6eeb4a8675780e119636488b4e3cfe5d34859a72a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
+    tags: ["React Js", "API", "Axios", "Node JS"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Breaking-Bad",
+    webapp: "https://breaking-bad-webapp.netlify.app",
+  },
+  {
+    id: 7,
+    title: "Quiz App",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "A android quiz app made with Java and Firebase. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the quiz categories and can create a new quiz. It has a leaderboard where users can see the top 10 scorers. It has a search bar where users can search for a particular quiz.",
+    image:
+      "https://github-production-user-asset-6210df.s3.amazonaws.com/64485885/239726262-c1b061d1-d9d0-42ef-9f1c-0412d14bc4f6.gif",
+    tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
+    category: "android app",
+    github: "https://github.com/rishavchanda/Quiz-Earn",
+    webapp: "https://github.com/rishavchanda/Quiz-Earn",
+  },
+  {
+    id: 8,
+    title: "Face Recognition",
+    date: "Jan 2021",
+    description:
+      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+    image:
+      "https://dontrepeatyourself.org/media/face-recognition-with-python-dlib-and-deep-learning_cezKZBj.png",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+];
+
